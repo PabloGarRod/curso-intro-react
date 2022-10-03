376df05edccfa22a7ac3bd6fb894e71353223636
Pablo <pablogarcirodriguez@gmail.com>
2022-10-03 18:59:35 +0200

    Clase 9: Manejo de estado

--- to-do-app/src/TodoSearch.js
+++ to-do-app/src/TodoSearch.js
@@ -2,16 +2,22 @@ import React from "react";
 import "./TodoSearch.css";
 
 function TodoSearch(){
-    const onSearchValueChange = (event) => {
-console.log(event.target.value);
-    }
-    return(
+const [searchValue, setSearchValue] = React.useState('');
+
+     const onSearchValueChange = (evento) => {
+        console.log(evento.target.value);
+        setSearchValue(evento.target.value);
+     }
+    return[
         <input 
         className="TodoSearch" 
         placeholder='Cebolla'
+        value={searchValue}
         onChange={onSearchValueChange}
-        />
-    )
+        />,
+
+        <p>{searchValue}</p>
+    ]
 }
 
 export {TodoSearch}
