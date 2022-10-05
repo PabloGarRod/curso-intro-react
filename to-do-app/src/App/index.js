2b619c0811803ede62c8b4692ff37b5f0dbbd03b
Pablo <pablogarcirodriguez@gmail.com>
2022-10-05 21:04:12 +0200

    Clase 14: Custom Hook para Local Storage

--- to-do-app/src/App/index.js
+++ to-do-app/src/App/index.js
@@ -9,19 +9,36 @@ import { AppUI } from './AppUI';
 //   {text: 'Salir de Bronce', completed: false},
 // ]
 
-function App() {
-  const localStorageTodos = localStorage.getItem('TODOS_V1');
-  let parsedTodos;
+function useLocalStorage(itemName, initialValue){
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
 
-  if (!localStorageTodos) {
-    localStorage.setItem('TODOS_V1', JSON.stringify([]));
-    parsedTodos = [];
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
   }else{
-    parsedTodos = JSON.parse(localStorageTodos);
+    parsedItem = JSON.parse(localStorageItem);
   }
+
+  const [item, setItem] = React.useState(parsedItem);
+
+  const saveItem= (newItem) => {
+    const stringifiedItem = JSON.stringify(newItem);
+    localStorage.setItem(itemName, stringifiedItem);
+    setItem(newItem);
+  };
+
+  return [
+    item,
+    saveItem,
+  ]
   
-  
-  const [todos, setTodos] = React.useState(parsedTodos);
+}
+
+function App() {
+  const [patito, savePatito] = useLocalStorage('PATITO_V1', 'FERNANDO');
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => todo.completed).length;
@@ -39,11 +56,6 @@ function App() {
         })
   }
 
-  const saveTodos= (newTodos) => {
-    const stringifiedTodos = JSON.stringify(newTodos);
-    localStorage.setItem('TODOS_V1', stringifiedTodos);
-    setTodos(newTodos);
-  };
 
   const completeTodo = (text) => {
       const todoIndex = todos.findIndex(todo => todo.text === text);
@@ -59,7 +71,8 @@ function App() {
     saveTodos(newTodos);
  }
 
-  return (
+  return [
+    <p>{patito}</p>,
     <AppUI
     totalTodos = {totalTodos}
     completedTodos = {completedTodos}
@@ -69,8 +82,8 @@ function App() {
     completeTodo = {completeTodo}
     deleteTodo = {deleteTodo}
 
-    />
-  );
+    />,
+  ];
 }
 
 
