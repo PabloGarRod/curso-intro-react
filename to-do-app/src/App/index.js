77608696f6afc0aa7b574ea8809f6b51f4c20933
Pablo <pablogarcirodriguez@gmail.com>
2022-10-05 20:00:22 +0200

    Clase 13: Persistencia de datos con Local Storage

--- to-do-app/src/App/index.js
+++ to-do-app/src/App/index.js
@@ -1,18 +1,27 @@
 import  React  from 'react';
 import { AppUI } from './AppUI';
-//import logo from './logo.svg';
-//import './App.css';
 
-const defaultTodos = [
-  {text: 'Cortar cebollas', completed: true},
-  {text: 'Tomar curso react', completed: false},
-  {text: 'Llorar en la llorería', completed: false},
-  {text: 'Cenar', completed: false},
-  {text: 'Salir de Bronce', completed: false},
-]
+// const defaultTodos = [
+//   {text: 'Cortar cebollas', completed: true},
+//   {text: 'Tomar curso react', completed: false},
+//   {text: 'Llorar en la llorería', completed: false},
+//   {text: 'Cenar', completed: false},
+//   {text: 'Salir de Bronce', completed: false},
+// ]
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  }else{
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+  
+  
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => todo.completed).length;
@@ -30,18 +39,24 @@ function App() {
         })
   }
 
+  const saveTodos= (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setTodos(newTodos);
+  };
+
   const completeTodo = (text) => {
       const todoIndex = todos.findIndex(todo => todo.text === text);
       const newTodos = [...todos]
       newTodos[todoIndex].completed = true;
-      setTodos(newTodos);
+      saveTodos(newTodos);
    }
 
    const deleteTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos]
     newTodos.splice(todoIndex, 1)
-    setTodos(newTodos);
+    saveTodos(newTodos);
  }
 
   return (
